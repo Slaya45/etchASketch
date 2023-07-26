6b07dbf158a17746ae047ef56b4333b45dee45b3
Slaya45 <caydenfrye@gmail.com>
2023-07-25 21:27:23 -0600

    positioning the buttons to the bottom of the sketch box, having trouble making it overlay correclty. going to try a new method next.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function generateChart() {
       let div = document.createElement("div");
       document.getElementById("mainContainer").appendChild(div);
 
-      div.style.width = "2.49vh";
-      div.style.height = "2.49vh";
+      div.style.width = "2.5vh";
+      div.style.height = "2.5vh";
       div.style.background = "rgb(199, 199, 199)";
 
       div.addEventListener("mouseover", function(){
