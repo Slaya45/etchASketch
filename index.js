dc5140071b0f13a871f6a0081e144801b88310b4
Slaya45 <caydenfrye@gmail.com>
2023-07-25 19:58:26 -0600

    moved my divs over to js to be dynamically generated using a for loop, then created the template-grid in css to create the 16x16 grid needed to make the etch a sketch

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function generateChart() {
+    for (let i = 0; i < 256; i++) {
+      let div = document.createElement("div");
+      document.getElementById("mainContainer").appendChild(div);
+    }
+  }
+  
+  generateChart();
