c3ec444f539b8e928ba9b73155a1890e0b34793a
Slaya45 <caydenfrye@gmail.com>
2023-07-25 20:48:57 -0600

    created a mouseover function that creates an event listener for the divs then removes it once the event takes place, then added some style to the box, making a grey background and startign to create buttons for style of the sketch. leaving it open to make these function as well.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 
 function generateChart() {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < 1024; i++) {
       let div = document.createElement("div");
       document.getElementById("mainContainer").appendChild(div);
+
+      div.style.width = "2.49vh";
+      div.style.height = "2.49vh";
+      div.style.background = "rgb(199, 199, 199)";
+
+      div.addEventListener("mouseover", function(){
+        this.style.background = "black";
+        this.removeEventListener("mouseover", arguments.callee);
+      })
     }
   }
   
   generateChart();
 
-
-
-  
  
