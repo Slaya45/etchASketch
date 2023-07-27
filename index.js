af6e03c1954229b22e5f71656147b77f71c6b15f
Slaya45 <caydenfrye@gmail.com>
2023-07-26 19:59:15 -0600

    Added a dropdown menue to the gris size button to allow the user to choose a diffrent gris size. I then added a function to clear the board and added this to the clear button. Also added comments to all of the code for better understanding for myslef as well as others

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
 
-document.addEventListener("DOMContentLoaded", function(){
-  generateChart(32);
-  console.log(test)
-})
+document.addEventListener("DOMContentLoaded", function() {
+  generateChart(numberGridBlocks);
+});
 
 
+let numberGridBlocks = 0;
+
+//Changes the size of the grid according to the options listed
+function changeGridSize(size) {
+  numberGridBlocks = size;
+  generateChart(numberGridBlocks);
+}
+function clearBoard(){
+let gridBlock = document.querySelectorAll("#board > div");
+
+// Remove all existing grid blocks from the board
+gridBlock.forEach((div) => {
+  div.remove();
+})};
+
+
+//Generates a chart/grid inside the main container
 function generateChart(size) {
-    
-      let board = document.querySelector("#board")
+  let board = document.querySelector("#board");
 
-      board.style.background = "rgb(199, 199, 199)";
-      board.style.gridTemplateColumns = `repeat(${size} 1fr)`;
-      board.style.gridTemplateRows = `repeat(${size} 1fr)`;
+  board.style.background = "rgb(199, 199, 199)";
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-      let numDivs = size * size;
+  let numDivs = size * size;
 
-      for (let i = 0; i < numDivs; i++) {
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    board.insertAdjacentElement("beforeend", div);
+  }
 
-        let div = document.createElement("div");
+//Selects all of the grid blocks and tracks a mouseover, when activated it changes the color and removes the listener
+  let gridBlocks = document.querySelectorAll("#board > div");
+  gridBlocks.forEach((div) => {
+    div.addEventListener("mouseover", function() {
+      this.style.background = "black";
+      this.removeEventListener("mouseover", arguments.callee);
+    });
+  });
+}
 
-        board.insertAdjacentElement("beforeend". div);
-        
-  
-      div.addEventListener("mouseover", function(){
-        this.style.background = "black";
-        this.removeEventListener("mouseover", arguments.callee);
-      })
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function gridDropdownList() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
   }
-  
-  generateChart();
+}
 
- 
